7d7084ced9259b0a217083a282f2309b5200e82c
Jeff Kerpan <jkerpan@gmail.com>
2017-05-25 12:33:47 -0600

    Checklist written and changed code with Katie

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-  console.log('Listening on Port: 3000');
-});
+// app.listen(3000, function() {
+//   console.log('Listening on Port: 3000');
+// });
+//
+// app.use(express.static('public'));
+//
+// app.get('/:wrong', function(req, res) {
+//   if (req.params.wrong === 'api') {
+//     res.send('Response from api');
+//   }else {
+//     res.status(404);
+//     res.send('404 Not Found');
+//   }
+// });
 
 app.use(express.static('public'));
 
-app.get('/:wrong', function(req, res) {
-  if (req.params.wrong === 'api') {
-    res.send('Response from api');
-  }else {
-    res.status(404);
-    res.send('404 Not Found');
-  }
+app.get('/api', function(req, res) {
+  res.send('Hello from API');
+});
+
+app.use(function(req, res) {
+  res.status(404).send('Not Found');
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port: 3000');
 });
